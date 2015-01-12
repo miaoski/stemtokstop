e843c20cf4ed44349834f669670e80d7a0ffad62
Lin Zhemin <lin.zhemin@gmail.com>
2015-01-12 19:42:53 +0800

    use request.post().json()

--- test.py
+++ test.py
@@ -4,7 +4,11 @@ import json
 
 rus_text = u'Пролетариат использует свое политическое господство для того, чтобы вырвать у буржуазии шаг за шагом весь капитал, централизовать все орудия производства в руках государства, т. е. пролетариата, организованного как господствующий класс, и возможно более быстро увеличить сумму производительных сил.'
 
-print "Sent:", rus_text
+print 'Sent:', rus_text
 r = requests.post('http://localhost:5000/ru', data = {'text': rus_text})
-j = json.loads(r.text)
-print "Recv:", ' '.join(j)
+j = r.json()
+print 'Recv:', ' '.join(j['text'])
+
+print 'Sending garbabe'
+r = requests.post('http://localhost:5000/ru', data = {'nothing': None})
+print 'Recv:', r

890ea6708fc2dd23c3f6a4a9c62db3e905a1f005
Lin Zhemin <lin.zhemin@gmail.com>
2015-01-12 19:50:18 +0800

    Use functional pointer

--- stemtokstop.py
+++ stemtokstop.py
@@ -1,5 +1,5 @@
 # -*- coding: utf8 -*-
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, abort
 from nltk.tokenize.punkt import PunktWordTokenizer
 from nltk.corpus import stopwords
 from nltk.stem import SnowballStemmer
@@ -8,21 +8,41 @@ import json
 app = Flask(__name__)
 JSON_MIME = 'application/json; charset=utf8'
 
-# ISO639-1
-NLTKlanguages={'NL': "dutch",
-               'FI': "finnish",
-               'DE': "german",
-               'IT': "italian",
-               'PT': "portuguese",
-               'ES': "spanish",
-               'TR': "turkish",
-               'DA': "danish",
-               'EN': "english",
-               'FR': "french",
-               'HU': "hungarian",
-               'NO': "norwegian",
-               'RU': "russian",
-               'SV': "swedish"}
+def dutch(sent):
+    pass
+
+def finnish(sent):
+    pass
+
+def german(sent):
+    pass
+
+def italian(sent):
+    pass
+
+def portuguese(sent):
+    pass
+
+def spanish(sent):
+    pass
+
+def turkish(sent):
+    pass
+
+def danish(sent):
+    pass
+
+def english(sent):
+    pass
+
+def french(sent):
+    pass
+
+def hungarian(sent):
+    pass
+
+def norwegian(sent):
+    pass
 
 def russian(sent):
     stem = SnowballStemmer('russian')
@@ -32,15 +52,37 @@ def russian(sent):
     px = [x for x in mx if x not in stop]
     return px
 
+def swedish(sent):
+    pass
+
+# ISO639-1
+NLTKlanguages={'nl': dutch,
+               'fi': finnish,
+               'de': german,
+               'it': italian,
+               'pt': portuguese,
+               'es': spanish,
+               'tr': turkish,
+               'da': danish,
+               'en': english,
+               'fr': french,
+               'hu': hungarian,
+               'no': norwegian,
+               'ru': russian,
+               'sv': swedish}
 
 @app.route("/")
 def hello():
     return "Tokenize, stemming, removing stop words.\n"
 
 
-@app.route("/ru", methods=['POST'])
-def post_russian():
-    js = russian(request.form['text'])
+@app.route("/<lang>", methods=['POST'])
+def post_lang(lang):
+    lang = lang.lower()
+    if lang not in NLTKlanguages:
+        abort(404)
+    func = NLTKlanguages[lang]
+    js = func(request.form['text'])
     return jsonify(text=js)
 
 
